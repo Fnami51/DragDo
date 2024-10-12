923b865f236feacc4b797edae89d64aa0766fb86
Fnami51 <154996881+Fnami51@users.noreply.github.com>
2024-10-12 10:44:24 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,13 @@
 import React, { StrictMode } from 'react';
-import { DndProvider } from 'react-dnd';
-import { TouchBackend } from 'react-dnd-touch-backend';
 import ItemsProvider from './src/context/ItemContext';
 import Main from './src/main';
 
 export default function App() {
   return (
     <StrictMode>
-    <DndProvider backend={TouchBackend}>
       <ItemsProvider>
         <Main />
       </ItemsProvider>
-    </DndProvider>
     </StrictMode>
   );
 }
